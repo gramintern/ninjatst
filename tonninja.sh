d8d164b6a3d4ee009ff890e4871f51ac4a00ac5c
Gram Intern <95147690+gramintern@users.noreply.github.com>
2024-02-06 23:32:33 +0200

    Add files via upload

---
+++ tonninja.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQBu3Mi0YgcWx1zFTkRGLt1SgQ9T3dXF7JWTiFEu-_20-ivZ -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
